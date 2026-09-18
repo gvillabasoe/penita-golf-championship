15b4d8fb8769d05346c4647ba240ad055e846386
gvillabasoe <g.villabasoesc@gmail.com>
2026-09-18 13:57:15 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-import { requireSession } from '@/lib/auth/server';
-
-export default async function HomePage() {
-  await requireSession('/');
-  redirect('/tarjeta');
-}
